c88c7fe791640c80222d5f00c383f4b63460137c
Parkhom95 <simbian19@gmail.com>
2024-09-09 16:18:44 +0500

    12341234

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import sqlite3
 import hashlib
+import os
+from werkzeug.utils import secure_filename #  отвечает за
 
 app = Flask(__name__)
 app.secret_key = 'password'
+
+# Путь для сохранения изображений
+path_to_save_images = os.path.join(app.root_path, 'static', 'imgs')
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_db_conn():
        conn = sqlite3.connect('database.db')
@@ -13,7 +24,31 @@ def get_db_conn():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    conn = get_db_conn()
+    blocks = conn.execute('SELECT * FROM content').fetchall()
+    conn.close()
+
+    blocks_list = [dict(ix) for ix in blocks]
+    # print(blocks_list)
+
+    json_data = {}
+    
+    for raw in blocks_list:
+        if raw['idblock'] not in json_data:
+            json_data[raw['idblock']] = []
+
+        json_data[raw['idblock']].append({
+            'id': raw['id'],
+            'short_title': raw['short_title'],
+            'img': raw['img'],
+            'altimg': raw['altimg'],
+            'title': raw['title'],
+            'contenttext': raw['contenttext'],
+            'author': raw['author'],
+            'timestampdata': raw['timestampdata']
+        })
+
+    return render_template('index.html', json_data=json_data)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -76,7 +111,42 @@ def admin():
 
         print(json_data)
         return render_template('admin.html', json_data = json_data)
-        
+
+
+@app.route('/update_content', methods=['POST'])
+def update_content():
+    if request.method == "POST":
+        content_id = request.form['id']
+        short_title = request.form['short_title']
+        title = request.form['title']
+        altimg = request.form.get('altimg')
+        contenttext = request.form['contenttext']
+    else:
+        return redirect(url_for("admin.html")) 
+
+    # Обработка загруженного файла
+    file = request.files['img']
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(path_to_save_images, filename)
+        imgpath = "/static/imgs/"+filename
+        file.save(save_path)
+        # Обновите путь изображения в вашей базе данных
+
+    # Обновление данных в базе
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    if file:
+        cursor.execute('UPDATE content SET short_title=?, img=?, altimg=?, title=?, contenttext=? WHERE id=?',
+                   (short_title, imgpath, altimg, title, contenttext, content_id))
+    else:
+        cursor.execute('UPDATE content SET short_title=?, altimg=?, title=?, contenttext=? WHERE id=?',
+                       (short_title, altimg, title, contenttext, content_id))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
